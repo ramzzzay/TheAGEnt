2c9296a47beb76a178deba7cc8bb98a6eeeece8d
ramzzzay <ramzzzay@gmail.com>
2016-10-05 18:25:22 +0300

    Add: User Manager Interface

---
+++ TheAGEnt.Domain/Abstract/IMainUserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using TheAGEnt.Domain.Entities;
+
+namespace TheAGEnt.Domain.Abstract
+{
+    interface IMainUserManager
+    {
+        Task<IdentityResult> ChangePasswordAsync(string email, string currentPassword, string newPassword);
+        Task<IdentityResult> CreateAsync(User u);
+        Task<List<User>> ShowAsync();
+        Task<IdentityResult> AddClaimToUserAsync(string email, string claimName);
+        Task<User> FindAsync(string name, string password);
+        Task<ClaimsIdentity> CreateIdentityAsync(User user, string authenticationType);
+        Task<User> FindByIdAsync(string userId);
+        Task<User> FindByEmailAsync(string email);
+        Task<IdentityResult> AddPasswordAsync(string userId, string newPassword);
+        Task<IdentityResult> AddLoginAsync(string userId, UserLoginInfo userInfo);
+        Task<IdentityResult> RemovePasswordAsync(string userId);
+        Task<IdentityResult> RemoveLoginAsync(string userId, UserLoginInfo userInfo);
+        Task<User> FindAsync(UserLoginInfo userLoginInfo);
+        Task<IdentityResult> UpdateAsync(User u);
+        IQueryable<User> FindByFirstName(string firstName);
+    }
+}
