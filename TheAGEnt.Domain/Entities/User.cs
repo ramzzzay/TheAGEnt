53194f7a4ff2c829efd46760adea8e281cc4d35f
ramzzzay <ramzzzay@gmail.com>
2016-10-05 18:25:07 +0300

    Add: user entity

---
+++ TheAGEnt.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TheAGEnt.Domain.Abstract;
+
+namespace TheAGEnt.Domain.Entities
+{
+    class User : IdentityUser
+    {
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(IMainUserManager manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            return userIdentity;
+        }
+    }
+}
