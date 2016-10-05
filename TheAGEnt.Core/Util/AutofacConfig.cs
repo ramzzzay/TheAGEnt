fe7a39c21c943909508552b56aedd159a86ca8f5
ramzzzay <ramzzzay@gmail.com>
2016-10-05 18:07:45 +0300

    Add: autofac config

---
+++ TheAGEnt.Core/Util/AutofacConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
+
+namespace TheAGEnt.Core.Util
+{
+    public class AutofacConfig
+    {
+        public static void Configure(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            var config = new HttpConfiguration();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+        }
+    }
+}
