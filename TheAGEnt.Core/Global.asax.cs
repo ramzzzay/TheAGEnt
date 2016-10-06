a665b10ab3dc702aacf83a0f723aef1429f768db
ramzzzay <ramzzzay@gmail.com>
2016-10-06 13:15:46 +0300

    Add: Autofac to Global config

--- TheAGEnt.Core/Global.asax.cs
+++ TheAGEnt.Core/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using TheAGEnt.Core.Util;
 
 namespace TheAGEnt.Core
 {
@@ -13,6 +14,8 @@ namespace TheAGEnt.Core
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            AutofacConfig.Configure();
         }
     }
 }
