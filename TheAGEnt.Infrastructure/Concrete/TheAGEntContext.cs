05cedd95655cbf8726137ef78b0f881c85179433
ramzzzay <ramzzzay@gmail.com>
2016-10-06 13:17:01 +0300

    Add: data base

---
+++ TheAGEnt.Infrastructure/Concrete/TheAGEntContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using TheAGEnt.Domain.Entities;
+
+namespace TheAGEnt.Infrastructure.Concrete
+{
+    public class TheAGEntContext : IdentityDbContext<User>
+    {
+        public TheAGEntContext() : base("TheAGEntContext", true)
+        {
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        public TheAGEntContext Create()
+        {
+            return new TheAGEntContext();
+        }
+    }
+}
---
+++ TheAGEnt.Infrastructure/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace TheAGEnt.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TheAGEnt.Infrastructure.Concrete.TheAGEntContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TheAGEnt.Infrastructure.Concrete.TheAGEntContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
