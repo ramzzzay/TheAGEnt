6bbbd0e97d5689fd468c3056779b2eb0541352ad
ramzzzay <ramzzzay@gmail.com>
2016-10-07 17:46:54 +0300

    Add: registration view

--- TheAGEnt.Core/Assets/js/components/Registration.jsx
+++ TheAGEnt.Core/Assets/js/components/Registration.jsx
@@ -1,6 +1,7 @@
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
-module.exports = React.createClass({
+let RegistrationForm = React.createClass({
     getInitialState: function() {
         return {
             email: "",
@@ -11,16 +12,87 @@ module.exports = React.createClass({
             password: ""
         };
     },
+    clearForm: function() {
+        this.setState({
+            email: "",
+            name: "",
+            surname: "",
+            nickname: "",
+            address: "",
+            password: "",
+            confirmPassword: ""
+        });
+    },
+    _emailFieldChange: function(e) {
+        this.setState({email: e.target.value});
+    },
+    _nameFieldChange: function(e) {
+        this.setState({name: e.target.value});
+    },
+    _surnameFieldChange: function(e) {
+        this.setState({surname: e.target.value});
+    },
+    _nicknameFieldChange: function(e) {
+        this.setState({nickname: e.target.value});
+    },
+    _addressFieldChange: function(e) {
+        this.setState({address: e.target.value});
+    },
+    _passwordFieldChange: function(e) {
+        this.setState({password: e.target.value});
+    },
+    _confirmPasswordFieldChange: function(e) {
+        this.setState({confirmPassword: e.target.value});
+    },
+    sendToServer: function() {
+        if (this.state.password === this.state.confirmPassword) {
+            var data = {
+                email: this.state.email,
+                name: this.state.name,
+                surname: this.state.surname,
+                nickname: this.state.nickname,
+                address: this.state.address,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            };
+
+        console.log(data);
+        fetch('api/Account/Register', {
+            method: 'POST',
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Authorization": "bearer " + Cookie.load('tokenInfo')
+            }),
+            body: JSON.stringify(data)
+        }).then(() => {
+            this.clearForm();
+        });
+        }
+    },
     render: function() {
         return (
             <div className="Registration">
-                <TextField hintText="Email" floatingLabelText="Enter please You email here"/><br/>
-                <TextField hintText="Name" floatingLabelText="Enter please You name here"/><br/>
-                <TextField hintText="Surname" floatingLabelText="Enter please You surname here"/><br/>
-                <TextField hintText="Nickname" floatingLabelText="Enter please You nickname here"/><br/>
-                <TextField hintText="Adress" floatingLabelText="Enter please You adress here"/><br/>
-                <TextField hintText="Password" floatingLabelText="Enter please You password here" type="password"/><br/>
-                <TextField hintText="Password" floatingLabelText="Re-enter please You password here" type="password"/><br/>
+                <TextField value={this.state.email} onChange={this._emailFieldChange} hintText="Email" floatingLabelText="Enter please You email here" type="email"/><br/>
+                <TextField value={this.state.name} onChange={this._nameFieldChange} hintText="Name" floatingLabelText="Enter please You name here"/><br/>
+                <TextField value={this.state.surname} onChange={this._surnameFieldChange} hintText="Surname" floatingLabelText="Enter please You surname here"/><br/>
+                <TextField value={this.state.nickname} onChange={this._nicknameFieldChange} hintText="Nickname" floatingLabelText="Enter please You nickname here"/><br/>
+                <TextField value={this.state.address} onChange={this._addressFieldChange} hintText="Adress" floatingLabelText="Enter please You adress here"/><br/>
+                <TextField value={this.state.password} onChange={this._passwordFieldChange} hintText="Password" floatingLabelText="Enter please You password here" type="password"/><br/>
+                <TextField value={this.state.confirmPassword} onChange={this._confirmPasswordFieldChange} hintText="Confirm Password" floatingLabelText="Re-enter please You password here" type="password"/><br/>
+                <RaisedButton label="Ok, let's start!" primary={true} onClick={this.sendToServer}/>
+            </div>
+        );
+    }
+});
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {foo: ""};
+    },
+    render: function() {
+        return (
+            <div className="Registration">
+                <RegistrationForm/>
             </div>
         );
     }
--- TheAGEnt.Core/Assets/js/containers/App.jsx
+++ TheAGEnt.Core/Assets/js/containers/App.jsx
@@ -12,22 +12,101 @@ import MenuItem from 'material-ui/MenuItem';
 import Drawer from 'material-ui/Drawer';
 
 import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import {fullWhite} from 'material-ui/styles/colors';
+import {Popover, PopoverAnimationVertical} from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+
+import Dialog from 'material-ui/Dialog';
+
+
+// TODO: Why this not work? I must fix that!
+let loginForm = React.createClass({
+    getInitialState: function() {
+        return {email: "", password: ""};
+    },
+    _emailFieldChange: function(e) {
+        this.setState({email: e.target.value});
+    },
+    render: function(){
+      return (
+        <div ref="loginForm">
+            <TextField value={this.state.email} onChange={this._emailFieldChange} hintText="Email" floatingLabelText="Enter please You email here" type="email"/><br/>
+        </div>
+      );
+    }
+});
 
 module.exports = React.createClass({
     getInitialState: function() {
-        return {open: false};
+        return {open: false, openUserMenu: false, openLoginWindow: false};
     },
     handleToggle: function() {
         this.setState({
             open: !this.state.open
         });
     },
-    handleClose: () => this.setState({open: false}),
+    handleClose: function() {
+        this.setState({open: false});
+    },
+
+    handleTouchTap: function(event) {
+        event.preventDefault();
+        this.setState({openUserMenu: true, anchorEl: event.currentTarget});
+    },
+    handleRequestClose: function() {
+        this.setState({openUserMenu: false});
+    },
+
+    handleOpenLoginWindow: function() {
+        this.setState({openLoginWindow: true});
+    },
+
+    handleCloseLoginWindow: function() {
+        this.setState({openLoginWindow: false});
+    },
     render: function() {
+        var actions = [ < FlatButton label = "Cancel" primary = {
+                true
+            }
+            onTouchTap = {
+                this.handleCloseLoginWindow
+            } />, < FlatButton label = "Submit" primary = {
+                true
+            }
+            keyboardFocused = {
+                true
+            }
+            onTouchTap = {
+                this.handleCloseLoginWindow
+            } />
+        ];
         return (
             <MuiThemeProvider>
                 <div className='container'>
-                    <AppBar title="AG" iconClassNameRight="muidocs-icon-navigation-expand-more" onLeftIconButtonTouchTap={this.handleToggle}/>
+                    <AppBar title="AG - You personal art place!" iconClassNameRight="muidocs-icon-navigation-expand-more" onLeftIconButtonTouchTap={this.handleToggle} iconElementRight={< FlatButton icon = { < MoreVertIcon />
+                    }
+                    onTouchTap = {
+                        this.handleTouchTap
+                    } />}>
+                        <Popover open={this.state.openUserMenu} anchorEl={this.state.anchorEl} anchorOrigin={{
+                            horizontal: 'left',
+                            vertical: 'bottom'
+                        }} targetOrigin={{
+                            horizontal: 'left',
+                            vertical: 'top'
+                        }} onRequestClose={this.handleRequestClose} animation={PopoverAnimationVertical}>
+                            <Menu>
+                                <MenuItem onTouchTap={this.handleOpenLoginWindow} primaryText="Log in"/>
+                                <MenuItem primaryText="Settings"/>
+                                <MenuItem primaryText="Sign out"/>
+                            </Menu>
+                        </Popover>
+                    </AppBar>
+                    <Dialog title="Dialog With Actions" actions={actions} modal={false} open={this.state.openLoginWindow} onRequestClose={this.handleCloseLoginWindow}>
+                        <loginForm/>
+                    </Dialog>
                     <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
                         <MenuItem href="/" onTouchTap={this.handleClose}>Home</MenuItem>
                         <MenuItem href="/AdminPanel" onTouchTap={this.handleClose}>Admin Panel</MenuItem>
