e89d23e898e06fe058ad1163b41121e46c49c5eb
ramzzzay <ramzzzay@gmail.com>
2016-10-07 15:01:25 +0300

    Add: Registration page

---
+++ TheAGEnt.Core/Controllers/RegistrationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TheAGEnt.Core.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: UserManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
