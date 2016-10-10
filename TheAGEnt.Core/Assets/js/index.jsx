2882afb147f97b7c24776f9a93e53c9d820940ba
ramzzzay <ramzzzay@gmail.com>
2016-10-10 17:48:55 +0300

    Add: settings view to route on client side

--- TheAGEnt.Core/Assets/js/index.jsx
+++ TheAGEnt.Core/Assets/js/index.jsx
@@ -2,6 +2,7 @@ import App from './containers/App';
 import MainPage from './components/MainPage';
 import AdminPanel from './components/AdminPanel';
 import Registration from './components/Registration';
+import Settings from './components/SettingsPage';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 
@@ -11,6 +12,7 @@ ReactDOM.render(
       <IndexRoute component={MainPage} />
       <Route path='AdminPanel' component={AdminPanel} />
       <Route path='Registration' component={Registration} />
+      <Route path='Settings' component={Settings} />
     </Route>
   </Router>,
   document.getElementById('root')
