e80adba067d9f458c9e7ed2e5b482beb27d6c3d6
ramzzzay <ramzzzay@gmail.com>
2016-10-11 17:33:37 +0300

    Add: Photos Controller

---
+++ TheAGEnt.Core/Controllers/PhotosController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
+using TheAGEnt.Core.Util;
+using TheAGEnt.Domain.Abstract;
+using TheAGEnt.Domain.Entities;
+
+namespace TheAGEnt.Core.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Photos")]
+    public class PhotosController : ApiController
+    {
+        private readonly IPhotoManager _photoManager;
+
+        public PhotosController()
+        {
+        }
+
+        public PhotosController(IPhotoManager photoManager)
+        {
+            _photoManager = photoManager;
+        }
+
+        // GET api/Photos/GetUserAlbumsById
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [Route("GetUserAlbumsById")]
+        [HttpGet]
+        [Authorize(Roles = "user")]
+        //public async Task<List<Album>> GetCurrentUserAlbums() => await _photoManager.GetUserAlbumsById(User.Identity.GetUserId());
+        public async Task<List<Album>> GetCurrentUserAlbums()
+        {
+            var alb = await _photoManager.GetUserAlbumsById(User.Identity.GetUserId());
+            var oalb = alb.Select(a=> a.Name);
+            var dbg = "dbg";
+            return alb;
+        }
+
+        // GET api/Photos/GetUserAlbumsNameById
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [Route("GetUserAlbumsNameById")]
+        [HttpGet]
+        [Authorize(Roles = "user")]
+        public IQueryable<string> GetUserAlbumsNameById() => _photoManager.GetUserAlbumsNameById(User.Identity.GetUserId());
+
+        // GET api/Photos/GetAnyUserAlbums
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [Route("GetAnyUserAlbums")]
+        [HttpGet]
+        [Authorize(Roles = "user")]
+        public async Task<List<Album>> GetAnyUserAlbums(string userEmail) => await _photoManager.GetUserAlbumsByEmail(userEmail);
+
+        [Route("UploadUserImage")]
+        // POST api/Photos/UploadUserImage
+        [MimeMultipart]
+        [Authorize(Roles = "user")]
+        public async Task<IHttpActionResult> UploadUserImage()
+        {
+            var uploadPath = HttpContext.Current.Server.MapPath("~/Assets/imgs/ProfileImages/Images");
+
+            var multipartFormDataStreamProvider = new UploadMultipartFormProvider(uploadPath);
+
+            // Read the MIME multipart asynchronously 
+            await Request.Content.ReadAsMultipartAsync(multipartFormDataStreamProvider);
+
+            var localFileName = multipartFormDataStreamProvider
+                .FileData.Select(multiPartData => multiPartData.LocalFileName).FirstOrDefault();
+
+            var userId = User.Identity.GetUserId();
+            var fileName = Path.GetFileName(localFileName);
+            var email = multipartFormDataStreamProvider.FormData["email"];
+            var album = multipartFormDataStreamProvider.FormData["album"];
+
+            var response = await _photoManager.ImageUpload(userId, $"/Assets/imgs/ProfileImages/Images/{fileName}", email, album);
+
+            var dbg = "dbg";
+
+            //var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+            //user.PathToPhoto = $"/Assets/imgs/ProfileImages/Images/{fileName}";
+
+            //var response = await UserManager.UpdateAsync(user);
+            //return response.Succeeded
+            //    ? Ok(new { Msg = response.Errors, IsOk = response.Succeeded, uploadedUrl = user.PathToPhoto })
+            //    : GetErrorResult(response);
+            return Ok(new { Msg = response.Errors, IsOk = response.Succeeded });
+        }
+    }
+}
