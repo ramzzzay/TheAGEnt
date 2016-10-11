88b575481514e5d99520014af1515fe2d4cd4726
ramzzzay <ramzzzay@gmail.com>
2016-10-11 21:56:16 +0300

    Update: main page view

--- TheAGEnt.Core/Assets/js/components/MainPage.jsx
+++ TheAGEnt.Core/Assets/js/components/MainPage.jsx
@@ -1,5 +1,55 @@
-﻿module.exports = React.createClass({
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around'
+    },
+    gridList: {
+        width: 500,
+        height: 450,
+        overflowY: 'auto'
+    }
+};
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {users: []};
+    },
+    getUsers: function() {
+      fetch('api/Account/GetAllUsersMiniInfo', {
+          method: 'GET',
+          headers: new Headers({
+              'Authorization': "bearer " + Cookie.load('tokenInfo'),
+              'Content-Type': "application/json"
+          })
+      }).then(r => r.json()).then(data => {
+          this.setState({users:data})
+      });
+    },
+    componentDidMount: function() {
+      this.getUsers();
+    },
     render: function() {
-        return (<div>Hello to main page!</div>);
+        return (
+            <div style={styles.root}>
+                <GridList cellHeight={180} style={styles.gridList}>
+                    <Subheader>Users</Subheader>
+                    {this.state.users.map((user) => (
+                        <GridTile key={user.img} title={user.Email} subtitle={< span > by < b > {
+                            user.Name
+                        } < /b></span >} actionIcon={< IconButton > <StarBorder color="white"/> < /IconButton>}> // TODO: go to links on click
+                            <img src={user.PathToPhoto}/>
+                        </GridTile>
+                    ))}
+                </GridList>
+            </div>
+        );
     }
 });
