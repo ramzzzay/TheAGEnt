5901fb778bb3066b48eaf6c40f8d3123f3ba4e6a
ramzzzay <ramzzzay@gmail.com>
2016-10-14 17:35:27 +0300

    Update: routes to hash mode

--- TheAGEnt.Core/Assets/js/index.jsx
+++ TheAGEnt.Core/Assets/js/index.jsx
@@ -1,34 +1,34 @@
 import App from './containers/App';
 import MainPage from './components/MainPage';
 import AdminPanel from './components/AdminPanel';
-import Registration from './components/Registration';
+/* import Registration from './components/Registration';*/
 import Settings from './components/SettingsPage';
 import UserAlbums from './components/UserAlbums';
 import UserPhotos from './components/UserPhotos';
 import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router'
 
-ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={MainPage} />
-      <Route name='AdminPanel' path='AdminPanel' component={AdminPanel}/>
-      <Route name='Registration' path='Registration' component={Registration}/>
-      <Route name='Settings' path='Settings' component={Settings}/>
-      <Route name='UserAlbums' path=':user' component={UserAlbums}/>
-      <Route name='UserPhotos' path=':user/:userAlbumName' component={UserPhotos}/>
-    </Route>
-  </Router>,
-  document.getElementById('root')
-);
-
 // ReactDOM.render(
-//   <Router history={hashHistory}>
+//   <Router history={browserHistory}>
 //     <Route path='/' component={App}>
 //       <IndexRoute component={MainPage} />
-//       <Route name='AdminPanel' path='AdminPanel' component={AdminPanel} handler={AdminPanel}/>
-//       <Route name='Registration' path='Registration' component={Registration} handler={Registration}/>
-//       <Route name='Settings' path='Settings' component={Settings} handler={Settings}/>
+//       <Route name='AdminPanel' path='AdminPanel' component={AdminPanel}/>
+//       <Route name='Settings' path='Settings' component={Settings}/>
+//       <Route name='UserAlbums' path=':user' component={UserAlbums}/>
+//       <Route name='UserPhotos' path=':user/:userAlbumName' component={UserPhotos}/>
 //     </Route>
 //   </Router>,
 //   document.getElementById('root')
 // );
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={MainPage} />
+      <Route name='AdminPanel' path='AdminPanel' component={AdminPanel} handler={AdminPanel}/>
+      <Route name='Settings' path='Settings' component={Settings} handler={Settings}/>
+      <Route name='UserAlbums' path=':user' handler={UserAlbums} component={UserAlbums}/>
+      <Route name='UserPhotos' path=':user/:userAlbumName' handler={UserPhotos} component={UserPhotos}/>
+    </Route>
+  </Router>,
+  document.getElementById('root')
+);
